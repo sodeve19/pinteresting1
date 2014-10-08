7df26ad661ce7487c4f4dacf051367e9e151649b
Sorel <sodeve619@gmail.com>
2014-10-08 13:23:16 -0500

    Some changes using bootstrap_WITHMYowncustomization

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
+  get "contact" => "pages#contact"
+  get "registro" => "pages#registro"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
